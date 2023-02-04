322e2593a28bae8e0635f0d8a71da74ec15e2176
EsraaArify <earify@provensolution.com>
2023-02-03 22:33:44 -0800

    Design

---
+++ app/src/main/java/com/example/graduationprojectjavamain/App_name_Activity.java
@@ -0,0 +1,14 @@
+package com.example.graduationprojectjavamain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class App_name_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_vid);
+    }
+}
---
+++ app/src/main/java/com/example/graduationprojectjavamain/Sign_UpActivity.java
@@ -0,0 +1,14 @@
+package com.example.graduationprojectjavamain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_UpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
